9f82c9c4d433cfd0c577299506842392dceaee2b
AlinaStratij <99261910+AlinaStratij@users.noreply.github.com>
2022-09-09 14:23:38 +0000

    Deploying to gh-pages from @ AlinaStratij/goit-js-hw-09@5dd6be9ea2677255c9c47506ebfe34a8ad995ec8 🚀

---
+++ 01-color-switcher.44a096cb.js
@@ -0,0 +1,2 @@
+!function(){var t={buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]")};console.log(t.buttonStart),console.log(t.buttonStop),t.buttonStart.addEventListener("click",(function(t){console.log("hello")})),t.buttonStop.addEventListener("click",onStopClick)}();
+//# sourceMappingURL=01-color-switcher.44a096cb.js.map
---
+++ 01-color-switcher.6beb3756.js
@@ -0,0 +1,2 @@
+const t={buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]")};console.log(t.buttonStart),console.log(t.buttonStop),t.buttonStart.addEventListener("click",(function(t){console.log("hello")})),t.buttonStop.addEventListener("click",onStopClick);
+//# sourceMappingURL=01-color-switcher.6beb3756.js.map
