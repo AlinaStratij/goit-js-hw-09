5dd6be9ea2677255c9c47506ebfe34a8ad995ec8
AlinaStratij <astratijcuk@gmail.com>
2022-09-09 17:23:08 +0300

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
 
+
+const refs = {
+  buttonStart: document.querySelector(`button[data-start]`),
+  buttonStop: document.querySelector(`button[data-stop]`),
+};
+console.log(refs.buttonStart);
+console.log(refs.buttonStop);
+
+refs.buttonStart.addEventListener(`click`,onStartClick)
+refs.buttonStop.addEventListener(`click`,onStopClick)
+
+function onStartClick(evt){
+  console.log(`hello`)
+}
